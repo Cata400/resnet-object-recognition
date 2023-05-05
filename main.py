98b68a7740a61d9f3258ce5ca1c2fbd81683a293
Cata400 <cata0310@gmail.com>
2023-05-06 02:15:29 +0300

    added model loading, training loop and result writing to csv

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 import os
 import torch
 
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from torchvision import transforms
 from torchvision.datasets import Caltech256
-from torchvision.models import resnet18, resnet50, resnet101, wide_resnet101_2, resnext101_64x4d, \
-    ResNet18_Weights, ResNet50_Weights, ResNet101_Weights, ResNeXt101_64X4D_Weights, Wide_ResNet101_2_Weights
-
+from torchvision.models import resnet18, resnet50, resnet101, wide_resnet101_2, resnext101_64x4d
 from utils import *
 
 
 if __name__ == '__main__':
     # Script parameters
-    dataset_path = os.path.join('..', 'Caltech_256')
-    classes = sorted(os.listdir(os.path.join(dataset_path, 'caltech256', '256_ObjectCategories')))
+    dataset_path = 'Caltech_256'
+    classes_path = os.path.join(dataset_path, 'caltech256', '256_ObjectCategories')
+    classes = sorted(os.listdir(classes_path))
     no_classes = len(classes)
     
-    save_model_name = 'model.h5'
+    save_model_name = 'resnet18_finetuned_full'
+    model_path = os.path.join('Models', save_model_name + '.pth')
+    
+    results_name = 'results_finetuned_full'
+    results_path = os.path.join('Results', results_name + '.csv')
 
     # Hyperparameters
-    batch_size = 32
+    batch_size = 4
     lr = 1e-3
     epochs = 50
     split = 0.7
@@ -29,10 +33,12 @@ if __name__ == '__main__':
     torch.backends.cudnn.benchmark = True
     
     # Load and preprocess data
+    # grayscale_imgs_count = check_dataset_shapes(classes_path) # TODO: fix grayscale images
+    # print(f'Grayscale images count: {grayscale_imgs_count}')
+    
     transform = transforms.Compose([
         transforms.Resize((512, 512)),
         transforms.ToTensor(), 
-        # transforms.Normalize((0.5,), (0.5,))
         ])
     
     dataset = Caltech256(root=dataset_path, 
@@ -53,15 +59,37 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f'Using {device}')
     
-    
     # Load model
+    model = resnet18(pretrained=True)
+    in_features_fc = model.fc.in_features
+    model.fc = torch.nn.Linear(in_features_fc, no_classes)
+    # model = torch.compile(model)
+    model.to(device)
+    print(f'Model number of parameters: {count_parameters(model)}')
     
+    # Train model, save model every 10 epochs, monitor it using tensorboard, keep track of computation time
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
     
-    # Train model using transfer learning, save model every 10 epochs and monitor it using tensorboard
+    model, test_accuracies, training_time = train(model, train_dataloader, test_loader, optimizer, criterion, epochs, device, lr_scheduler, save_model_name)
     
-    
-    # Load best model and evaluate it on test set
-    
+    # Write the accuracies and time into a csv file
+    results_df = pd.read_csv(results_path)
+    row = {
+            'Model': save_model_name, 
+            'Split': f"{int(split * 100)} - {int((1 - split) * 100)}", 
+            'Batch_size': batch_size,
+            'Initial_lr': lr,
+            'Accuracy_epoch_10': test_accuracies[0],
+            'Accuracy_epoch_20': test_accuracies[1],
+            'Accuracy_epoch_30': test_accuracies[2],
+            'Accuracy_epoch_40': test_accuracies[3],
+            'Accuracy_epoch_50': test_accuracies[4],
+            'Time': training_time
+        }
+    results_df = results_df.append(row, ignore_index=True)
+    results_df.to_csv(results_path, index=False)
     
     # Get confusion matrix
     
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 import torchvision
 
@@ -6,7 +7,92 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from torch.utils.tensorboard import SummaryWriter
 
+
+def train(model, train_loader, test_loader, optimizer, criterion, epochs, device, scheduler, save_model_name):
+    start = time.time()
+    test_accuracies = []
+    writer = SummaryWriter(log_dir='Logs')
+    
+    for epoch in range(epochs):
+        print(f'Epoch {epoch + 1}/{epochs}')
+        print('-' * 10)
+        
+        model.train()
+        train_iter_loss = 0.0
+        train_iter_acc = 0
+        
+        for x, y in train_loader:
+            x, y = x.to(device), y.to(device)
+            
+            optimizer.zero_grad()
+            
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            
+            loss.backward()
+            optimizer.step()
+            
+            train_iter_loss += loss.item() / len(train_loader)
+            train_iter_acc += (y_hat.argmax(1) == y.argmax(1)).type(torch.float).sum().item() / len(train_loader)
+        
+        scheduler.step()
+        print(f'Training loss: {train_iter_loss:.4f}\t Training accuracy: {train_iter_acc:.4f}')
+        writer.add_scalar('Loss/train', train_iter_loss, epoch)
+        writer.add_scalar('Accuracy/train', train_iter_acc, epoch)
+        
+        model.eval()
+        test_iter_loss = 0.0
+        test_iter_acc = 0
+        
+        for x, y in test_loader:
+            x, y = x.to(device), y.to(device)
+            
+            optimizer.zero_grad()
+                        
+            with torch.no_grad():
+                y_hat = model(x)
+                loss = criterion(y_hat, y)
+            
+            test_iter_loss += loss.item() / len(test_loader)
+            test_iter_acc += (y_hat.argmax(1) == y.argmax(1)).type(torch.float).sum().item() / len(test_loader)
+            
+        print(f'Test loss: {test_iter_loss:.4f}\t Test accuracy: {test_iter_acc:.4f}')
+        writer.add_scalar('Loss/test', test_iter_loss, epoch)
+        writer.add_scalar('Accuracy/test', test_iter_acc, epoch)
+        
+        if (epoch + 1) % 10 == 0:
+            torch.save(model.state_dict(), save_model_name + f'_{epoch + 1}.pth')
+            test_accuracies.append(test_iter_acc)
+        
+        print()
+        
+    stop = time.time()
+    print(f'Training time: {stop - start:.2f} seconds')
+    writer.flush()
+    writer.close()
+    
+    return model, test_accuracies, stop - start
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def check_dataset_shapes(dataset_path):
+    count = 0
+    for cls in os.listdir(dataset_path):
+        for img in os.listdir(os.path.join(dataset_path, cls)):
+            img_path = os.path.join(dataset_path, cls, img)
+            img = torchvision.io.read_image(img_path)
+            
+            if img.shape[0] != 3:
+                print(f'{img_path} has {img.shape[0]} channels')
+                count += 1
+                
+    return count
+                
 
 def get_confusion_matrix(y_true, y_pred, labels, plot_cm=True, scale=10, print_acc=False):
     """
